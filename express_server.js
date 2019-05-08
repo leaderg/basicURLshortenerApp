ae98f9e21e9951a5c396c5b5a66db1ed2f650fbd
leaderg <leader.g@gmail.com>
2019-05-08 20:33:19 +0000

    Adding user registration functionality.

--- express_server.js
+++ express_server.js
@@ -13,10 +13,44 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 //app.route() create chainable route handlers for a route path GET POST etc.
 app.get("/", (req, res) => {
   res.send("Help Me");
 });
+
+app.get("/register", (req, res) => {
+  let templateVariables = { username: req.cookies["username"] };
+  res.render("registration", templateVariables);
+});
+
+app.post("/register", (req, res) => {
+  if (inUsers(users, 'email', req.body.user)) {
+    res.status(400).send('email in use</br><a href="/register">Go Back</a>');
+  } else {
+    let newId = generateRandomString();
+    users[newId] = {
+      id: newId,
+      email: req.body.user,
+      password: req.body.pass
+    };
+    res.cookie("username", req.body.user);
+    console.log(users);
+    res.redirect("/urls");
+  }});
 
 app.get("/urls", (req, res) => {
   let templateVariables = { urls: urlDatabase, username: req.cookies["username"] };
@@ -32,7 +66,8 @@ app.post("/urls", (req, res) => {
   let shortenedURL = generateRandomString()
   urlDatabase[shortenedURL] = "http://" + req.body.longURL;
   let templateVars = { shortURL: shortenedURL, longURL: req.body.longURL, username: req.cookies["username"] };
-  res.render("urls_show", templateVars);
+  // res.render("urls_show", templateVars);
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -87,3 +122,12 @@ function generateRandomString() {
    }
    return result;
 }
+
+function inUsers(obj, key, value) {
+  for (let entry in obj) {
+    if (obj[entry][key] === value) {
+      return true;
+    };
+  }''
+  return false;
+};
